4dad94fdae66d5a1dd7f25c00f25d2178fd9cbb5
Conordoherty165 <75123069+Conordoherty165@users.noreply.github.com>
2021-11-16 14:44:21 +0000

    Update FileChoice.java

--- ImProcSoft/FileChoice.java
+++ ImProcSoft/FileChoice.java
@@ -26,12 +26,13 @@ public class FileChoice extends Frame implements ActionListener
    private Button btnClick7;
    private Button btnClick8;
    private Button btnClick9;
-
+   private Button btnClick10;
+   private Button btnClick11;
 
 
 
   //private GainSlider gS;  
-  
+
    static TextField entry1;
    static TextField entry2;
    private  Label prompt1;
@@ -54,72 +55,82 @@ public class FileChoice extends Frame implements ActionListener
       layout = new FlowLayout(FlowLayout.CENTER,90,30);
       setLayout(layout);
       setSize(500,600); 
-   
+
       entry1=new TextField(30);
       add(entry1);
       prompt1 = new Label("  Enter location of GIF/JPG File  ");
       add(prompt1);
-   
+
       entry2= new TextField(30);
       add(entry2);
       prompt2=new Label("   Enter the filter type you want or select from buttons below   ");
       add(prompt2);
-   
-   
-   
+
+
+
       btnClick1 = new Button("     Display Image and Filtered Image    ");
       add(btnClick1);
       btnClick1.addActionListener(this);
       show(); 
-   
+
       btnClick2 = new Button("AV_2x2");
       add(btnClick2);
       btnClick2.addActionListener(this);
       show();
-   
+
       btnClick3 = new Button("V_EDGE_2x2");
       add(btnClick3);
       btnClick3.addActionListener(this);
       show();
-   
+
       btnClick4 = new Button("LR_DIAG_2x2");
       add(btnClick4);
       btnClick4.addActionListener(this);
       show();
-   
+
       btnClick5 = new Button("toBW");
       add(btnClick5);
       btnClick5.addActionListener(this);
       show();
-   
+
       btnClick6 = new Button("Random");
       add(btnClick6);
       btnClick6.addActionListener(this);
       show();
-   
+
       btnClick7 = new Button("Sharp");
       add(btnClick7);
       btnClick7.addActionListener(this);
       show();
-   
+
       btnClick8 = new Button("Add 50");
       add(btnClick8);
       btnClick8.addActionListener(this);
       show();
-      
+
       btnClick9 = new Button("Poster");
       add(btnClick9);
       btnClick9.addActionListener(this);
       show();
-   
-   
-   }
- 
 
- 
+      btnClick10 = new Button("SquareRoot");
+      add(btnClick10);
+      btnClick10.addActionListener(this);
+      
+      btnClick11 = new Button("Blend");
+      add(btnClick11);
+      btnClick11.addActionListener(this);
+      
+      
+
+
+   }
+
+
+
    public static void main(String[] args)
    {FileChoice fc = new FileChoice();
-   
+
       fc.addWindowListener( 
          new WindowAdapter()
          {
@@ -128,7 +139,7 @@ public class FileChoice extends Frame implements ActionListener
             //fc.dispose();
             }
          });
-   
+
    } //end main
 
 
@@ -138,7 +149,7 @@ public class FileChoice extends Frame implements ActionListener
    {
       if (e.getActionCommand().equals("     Display Image and Filtered Image    "))
       {
-      
+
          temp1=entry1.getText();//temp1=entry1.getText();
          temp2=entry2.getText();
          System.out.println(temp1);
@@ -155,158 +166,197 @@ public class FileChoice extends Frame implements ActionListener
          s1.delete(0,60);
          s2.delete(0,60);
       }
-      
-      
+
+
       else if (e.getActionCommand().equals("AV_2x2"))
       {
          temp1=entry1.getText();
          temp2=entry2.getText();
-      
+
          s1.append(temp1);
          s2.append(temp2);
-      
+
          str1=s1.toString();
          str2="AV_2x2";
-      
+
          System.out.println("The filtertype is ...."+str2);
          DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
-      
-      
+
+
          s1.delete(0,60);
          s2.delete(0,60);
-      
+
       }
-      
+
       else if (e.getActionCommand().equals("Sharp"))
       {
          temp1=entry1.getText();
          temp2=entry2.getText();
-      
+
          s1.append(temp1);
          s2.append(temp2);
-      
+
          str1=s1.toString();
          str2="Sharp";
-      
+
          System.out.println("The filtertype is ...."+str2);
          DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
-      
-      
+
+
          s1.delete(0,60);
          s2.delete(0,60);
-      
+
       }
-      
-      
+
+
       else if (e.getActionCommand().equals("toBW"))
       {
          temp1=entry1.getText();
          temp2=entry2.getText();
-      
+
          s1.append(temp1);
          s2.append(temp2);
-      
+
          str1=s1.toString();
          str2="toBW";
-      
+
          System.out.println("The filtertype is ...."+str2);
          DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
-      
-      
+
+
          s1.delete(0,60);
          s2.delete(0,60);
-      
+
       }
-      
-      
+
+
       else if (e.getActionCommand().equals("V_EDGE_2x2"))
       {temp1=entry1.getText();temp1=entry1.getText();
       //temp2=entry2.getText();
-      
+
          s1.append(temp1);
          s2.append(temp2);
-      
-      
-      
+
+
+
          str1=s1.toString();
          str2="V_EDGE_2x2";
-      
+
          System.out.println("The filtertype is ...."+str2);
          DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
          s1.delete(0,60);
          s2.delete(0,60);
       }
-      
+
       else if (e.getActionCommand().equals("Random"))
       {temp1=entry1.getText();temp1=entry1.getText();
       //temp2=entry2.getText();
-      
+
          s1.append(temp1);
          s2.append(temp2);
-      
+
          str1=s1.toString();
          str2="Random";
-      
+
          System.out.println("The filtertype is ...."+str2);
          DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
          s1.delete(0,60);
          s2.delete(0,60);
       }
-      
+
       else if (e.getActionCommand().equals("GainSlider"))
       {temp1=entry1.getText();temp1=entry1.getText();
       //temp2=entry2.getText();
-      
+
          s1.append(temp1);
          s2.append(temp2);
-      
+
          str1=s1.toString();
          str2="GainSlider";
-      
+
          System.out.println("The filtertype is ...."+str2);
       //DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
       // gS=new GainSlider(str1);
          s1.delete(0,60);
          s2.delete(0,60);
       }
-      
+
       else if (e.getActionCommand().equals("Add 50")){
        temp1=entry1.getText();
          temp2=entry2.getText();
-      
+
          s1.append(temp1);
          s2.append(temp2);
-      
+
          str1=s1.toString();
          str2="Add 50";
-      
+
          System.out.println("The filtertype is ...."+str2);
          DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
-      
-      
+
+
          s1.delete(0,60);
          s2.delete(0,60);
 
-      
+
       }
-      
+
       else if (e.getActionCommand().equals("Poster")){
        temp1=entry1.getText();
          temp2=entry2.getText();
-      
+
          s1.append(temp1);
          s2.append(temp2);
-      
+
          str1=s1.toString();
          str2="Poster";
-      
+
          System.out.println("The filtertype is ...."+str2);
          DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
-      
-      
+
+
          s1.delete(0,60);
          s2.delete(0,60);
       }
-   
+
+      else if (e.getActionCommand().equals("SquareRoot")){
+          temp1=entry1.getText();
+            temp2=entry2.getText();
+
+            s1.append(temp1);
+            s2.append(temp2);
+
+            str1=s1.toString();
+            str2="Poster";
+
+            System.out.println("The filtertype is ...."+str2);
+            DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
+
+
+            s1.delete(0,60);
+            s2.delete(0,60);
+         }
+      
+      else if (e.getActionCommand().equals("Blend")){
+          temp1=entry1.getText();
+            temp2=entry2.getText();
+
+            s1.append(temp1);
+            s2.append(temp2);
+
+            str1=s1.toString();
+            str2="Blend";
+
+            System.out.println("The filtertype is ...."+str2);
+            DisplayFilterOutput dFO = new DisplayFilterOutput(str1,str2);
+
+
+            s1.delete(0,60);
+            s2.delete(0,60);
+
+
+         }
+
+
    }
 }
